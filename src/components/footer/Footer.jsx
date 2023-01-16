b7e5bd955a5ed114ec297b5484af9a5471d2db39
Tiara Temple <tmtemple7@gmail.com>
2023-01-16 14:05:08 -0600

    added more sections to footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import "./Footer.css";
+import { TbBuildingWarehouse } from "react-icons/tb";
+import { BsEnvelope } from "react-icons/bs";
+import { SlScreenSmartphone } from "react-icons/sl";
+import { HiOutlinePlus } from "react-icons/hi";
 
 function Footer() {
   return (
@@ -18,6 +22,34 @@ function Footer() {
           offers, promotions, other commercial messages. You can view our
           Privacy Policy here and you may unsubscribe at any time.
         </p>
+      </div>
+      <div className="store-email-texts-container">
+        <div className="store-section">
+          <TbBuildingWarehouse className="store-icon" />
+          <h4 className="store-locator">Store Locator</h4>
+        </div>
+        <div className="email-section">
+          <BsEnvelope className="email-icon" />
+          <h4 className="suf-email">Sign Up For Email</h4>
+        </div>
+        <div className="texts-section">
+          <SlScreenSmartphone className="phone-icon" />
+          <h4 className="suf-texts">Sign Up For Texts</h4>
+        </div>
+      </div>
+      <div className="help-ql-extras-container">
+        <div className="help-section">
+          <h4 className="help">Help</h4>
+          <HiOutlinePlus className="plus-icon" />
+        </div>
+        <div className="ql-section">
+          <h4 className="ql">Quick Links</h4>
+          <HiOutlinePlus className="plus-icon" />
+        </div>
+        <div className="extras-section">
+          <h4 className="extras">Extras</h4>
+          <HiOutlinePlus className="plus-icon" />
+        </div>
       </div>
     </footer>
   );
